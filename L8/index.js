b58e3df5a9f7f4e4e8c2344dfe12cb9b7ab52e92
MariamMEK <mariam.mekarishvili.1@btu.edu.ge>
2021-06-21 01:40:43 +0400

    655

---
+++ L8/index.js
@@ -0,0 +1,8 @@
+
+
+const span = document.getElementById("text");
+span.style.fontStyle = 'italic';
+span.style.textDecoration = 'underline';
+span.style.fomtWeight = 700;
+
+document.getElementById("first").innerHTML = span;
